2c2f977a769f29c908dfed04b325cd6466714eec
Jiang, Mengyu <jiang.work@hotmail.com>
2021-07-13 11:08:45 +0800

    unify entry-footer

--- template-parts/content.php
+++ template-parts/content.php
@@ -21,29 +21,6 @@
 
 	<?php twentysixteen_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-			the_content(
-				sprintf(
-					/* translators: %s: Post title. */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-					get_the_title()
-				)
-			);
-
-			wp_link_pages(
-				array(
-					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
-					'after'       => '</div>',
-					'link_before' => '<span>',
-					'link_after'  => '</span>',
-					'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
-					'separator'   => '<span class="screen-reader-text">, </span>',
-				)
-			);
-			?>
-	</div><!-- .entry-content -->
-
 	<footer class="entry-footer">
 		<?php twentysixteen_entry_meta(); ?>
 		<?php
